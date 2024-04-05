5108634a4ab2c766cef5ce99d328a60e326c8d6c
DongjinSon9 <dongj1358@naver.com>
2024-04-05 16:07:25 +0900

    7교시 - MemberDTO 생성해서 메서드에 적용 2

--- src/main/java/com/bitc/java404/Main.java
+++ src/main/java/com/bitc/java404/Main.java
@@ -42,14 +42,57 @@ public class Main {
     public static void main(String[] args) {
         dbTedst();
 
-        ParserToDB parser = new ParserToDB();
+//        ParserToDB parser = new ParserToDB();
 //        parser.insertDB();
+//        parser.selectDB();
+//        System.out.println("------데이터 삭제------");
+//        parser.deleteDB("test3");
+//        System.out.println("------데이터 수정------");
+//        parser.updateDB();
+//        parser.selectDB();
+
+
+        MemberDTO member = new MemberDTO();
+//        member.setUser_id("test4");
+//        member.setUser_pw("1234");
+//        member.setUser_name("테스트유저4");
+//        member.setUser_email("test4@bit.ac.kr");
+//        member.setUser_phone("01013572468");
+//        member.setUser_addr("서울시");
+
+        ParserToDB parser = new ParserToDB();
+//        System.out.println("---------기존 사용자 정보 확인---------");
+//        parser.selectDB();
+//        System.out.println("------------사용자 정보 추가-----------");
+//        parser.insertDB(member);
+//        System.out.println("---------추가된 사용자 정보 확인---------");
+//        parser.selectDB();
+
+        member.setUser_id("test5");
+        member.setUser_pw("4321");
+        member.setUser_name("테스트유저5");
+        member.setUser_email("test5@bit.ac.kr");
+        member.setUser_phone("01011223344");
+        member.setUser_addr("서울시");
+
+//        System.out.println("------------사용자 정보 추가-----------");
+//        parser.insertDB(member);
+//        System.out.println("---------추가된 사용자 정보 확인---------");
+//        parser.selectDB();
+
+        System.out.println("-----------데이터 수정하기-----------");
+
+        member.setUser_id("test5");
+        member.setUser_pw("123456789");
+        member.setUser_name("유저테스터5");
+        member.setUser_email("test5@gmail.com");
+        member.setUser_phone("01055667788");
+        member.setUser_addr("부산시");
+
+        parser.updateDB(member);
+        System.out.println("---------변경된 사용자 정보 확인---------");
         parser.selectDB();
-        System.out.println("------데이터 삭제------");
-        parser.deleteDB("test3");
-        System.out.println("------데이터 수정------");
-        parser.updateDB();
-        parser.selectDB();
+
 
     }
 }
